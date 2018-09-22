a5e7a33b2a3b328b42eca4cdb0605760ee1905d8
Danny Michaeli <sundayhd5@gmail.com>
2018-09-22 16:33:16 +0300

    changes made

--- lib/AbsoluteGrid.jsx
+++ lib/AbsoluteGrid.jsx
@@ -1,36 +1,46 @@
-'use strict';
+"use strict";
 
-import React, { Component, PureComponent } from 'react';
-import { debounce, sortBy } from 'lodash';
+import React, { Component, PureComponent } from "react";
+import { debounce, sortBy } from "lodash";
 
-import createDisplayObject from './BaseDisplayObject.jsx';
-import DragManager from './DragManager.js';
-import LayoutManager from './LayoutManager.js';
-import PropTypes from 'prop-types';
+import createDisplayObject from "./BaseDisplayObject.jsx";
+import DragManager from "./DragManager.js";
+import LayoutManager from "./LayoutManager.js";
+import PropTypes from "prop-types";
 
-export default function createAbsoluteGrid(DisplayObject, displayProps = {}, forceImpure = false) {
-
+export default function createAbsoluteGrid(
+  DisplayObject,
+  displayProps = {},
+  forceImpure = false
+) {
   const Comp = forceImpure ? Component : PureComponent;
-  const WrappedDisplayObject = createDisplayObject(DisplayObject, displayProps, forceImpure);
+  const WrappedDisplayObject = createDisplayObject(
+    DisplayObject,
+    displayProps,
+    forceImpure
+  );
 
   return class extends Comp {
     static defaultProps = {
       items: [],
-      keyProp: 'key',
-      filterProp: 'filtered',
-      sortProp: 'sort',
+      keyProp: "key",
+      filterProp: "filtered",
+      sortProp: "sort",
       itemWidth: 128,
       itemHeight: 128,
       verticalMargin: -1,
       responsive: false,
       dragEnabled: false,
-      animation: 'transform 300ms ease',
+      animation: "transform 300ms ease",
       zoom: 1,
-      onMove: ()=>{},
-      onDragStart: ()=>{},
-      onDragMove: ()=>{},
-      onDragEnd: ()=>{}
-    }
+      rtl: false,
+      minSort: 0,
+      maxSort: 9999,
+      onMove: () => {},
+      onDragStart: () => {},
+      onDragMove: () => {},
+      onDragEnd: () => {}
+    };
 
     static propTypes = {
       items: PropTypes.arrayOf(PropTypes.object).isRequired,
@@ -47,10 +57,13 @@ export default function createAbsoluteGrid(DisplayObject, displayProps = {}, for
       onMove: PropTypes.func,
       onDragStart: PropTypes.func,
       onDragMove: PropTypes.func,
-      onDragEnd: PropTypes.func
-    }
+      onDragEnd: PropTypes.func,
+      rtl: PropTypes.bool,
+      minSort: PropTypes.number,
+      maxSort: PropTypes.number
+    };
 
-    constructor(props, context){
+    constructor(props, context) {
       super(props, context);
       this.onResize = debounce(this.onResize, 150);
       this.dragManager = new DragManager(
@@ -58,15 +71,17 @@ export default function createAbsoluteGrid(DisplayObject, displayProps = {}, for
         this.props.onDragStart,
         this.props.onDragEnd,
         this.props.onDragMove,
-        this.props.keyProp);
+        this.props.keyProp
+      );
       this.state = {
-        layoutWidth: 0
+        layoutWidth: 0,
+        parentNode: null
       };
     }
 
     render() {
-      if(!this.state.layoutWidth || !this.props.items.length){
-        return <div ref={node => this.container = node}></div>;
+      if (!this.state.layoutWidth || !this.props.items.length) {
+        return <div ref={node => (this.container = node)} />;
       }
 
       let filteredIndex = 0;
@@ -78,16 +93,21 @@ export default function createAbsoluteGrid(DisplayObject, displayProps = {}, for
        This also clears out filtered items from the sort order and
        eliminates gaps and duplicate sorts
        */
-      sortBy(this.props.items, this.props.sortProp).forEach(item => {
-        if(!item[this.props.filterProp]){
+      const filteredItems = this.props.items.filter(
+        item =>
+          this.props.minSort == null ||
+          this.props.maxSort == null ||
+          (item.sort >= this.props.minSort && item.sort < this.props.maxSort)
+      );
+      sortBy(filteredItems, this.props.sortProp).forEach(item => {
+        if (!item[this.props.filterProp]) {
           const key = item[this.props.keyProp];
           sortedIndex[key] = filteredIndex;
           filteredIndex++;
         }
       });
-
-      const itemsLength = this.props.items.length;
-      const gridItems = this.props.items.map(item => {
+      const itemsLength = filteredItems.length;
+      const gridItems = filteredItems.map(item => {
         const key = item[this.props.keyProp];
         const index = sortedIndex[key];
         return (
@@ -106,6 +126,8 @@ export default function createAbsoluteGrid(DisplayObject, displayProps = {}, for
             filterProp={this.props.filterProp}
             dragEnabled={this.props.dragEnabled}
             dragManager={this.dragManager}
+            container={this.state.container}
+            rtl={this.props.rtl}
           />
         );
       });
@@ -118,8 +140,8 @@ export default function createAbsoluteGrid(DisplayObject, displayProps = {}, for
       };
       const layout = new LayoutManager(options, this.state.layoutWidth);
       const gridStyle = {
-        position: 'relative',
-        display: 'block',
+        position: "relative",
+        display: "block",
         height: layout.getTotalHeight(filteredIndex)
       };
 
@@ -127,7 +149,7 @@ export default function createAbsoluteGrid(DisplayObject, displayProps = {}, for
         <div
           style={gridStyle}
           className="absoluteGrid"
-          ref={node => this.container = node}
+          ref={node => (this.container = node)}
         >
           {gridItems}
         </div>
@@ -136,14 +158,33 @@ export default function createAbsoluteGrid(DisplayObject, displayProps = {}, for
 
     componentDidMount() {
       //If responsive, listen for resize
-      if(this.props.responsive){
-        window.addEventListener('resize', this.onResize);
+      if (this.props.responsive) {
+        window.addEventListener("resize", this.onResize);
       }
       this.onResize();
+      this.setContainer();
     }
 
     componentWillUnmount() {
-      window.removeEventListener('resize', this.onResize);
+      window.removeEventListener("resize", this.onResize);
+    }
+
+    getContainerParentNode() {
+      if (this.container != null && this.container.parentNode != null)
+        return this.container.parentNode;
+      else return null;
+    }
+
+    setContainer() {
+      if (this.container) {
+        this.setState({ container: this.container });
+      } else {
+        setTimeout(() => {
+          if (this.container) {
+            this.setState({ container: this.container });
+          }
+        }, 100);
+      }
     }
 
     onResize = () => {
@@ -152,17 +193,13 @@ export default function createAbsoluteGrid(DisplayObject, displayProps = {}, for
       } else {
         setTimeout(this.getDOMWidth, 66);
       }
-    }
+    };
 
     getDOMWidth = () => {
       const width = this.container && this.container.clientWidth;
-
-      if(this.state.layoutWidth !== width){
-        this.setState({layoutWidth: width});
+      if (this.state.layoutWidth !== width) {
+        this.setState({ layoutWidth: width });
       }
-
-    }
-
-
-  }
+    };
+  };
 }
--- lib/BaseDisplayObject.jsx
+++ lib/BaseDisplayObject.jsx
@@ -1,12 +1,15 @@
-'use strict';
+"use strict";
 
-import React, { Component, PureComponent } from 'react';
+import React, { Component, PureComponent } from "react";
 
-import LayoutManager from './LayoutManager.js';
-import PropTypes from 'prop-types';
+import LayoutManager from "./LayoutManager.js";
+import PropTypes from "prop-types";
 
-export default function createDisplayObject(DisplayObject, displayProps, forceImpure) {
-
+export default function createDisplayObject(
+  DisplayObject,
+  displayProps,
+  forceImpure
+) {
   const Comp = forceImpure ? Component : PureComponent;
 
   return class extends Comp {
@@ -16,55 +19,108 @@ export default function createDisplayObject(DisplayObject, displayProps, forceIm
       index: PropTypes.number,
       dragEnabled: PropTypes.bool,
       dragManager: PropTypes.object,
-      itemsLength: PropTypes.number
+      itemsLength: PropTypes.number,
+      container: PropTypes.object,
+      rtl: PropTypes.bool
+    };
+
+    constructor(props) {
+      super(props);
     }
 
-    state = {}
+    state = { dragX: null, dragY: null };
 
     updateDrag(x, y) {
       //Pause Animation lets our item return to a snapped position without being animated
       let pauseAnimation = false;
-      if(!this.props.dragManager.dragItem){
+      let endAnimation = false;
+      if (
+        !this.props.dragManager.dragItem &&
+        this.props.dragManager.dragItemStarted
+      ) {
+        endAnimation = true;
+        setTimeout(() => {
+          this.setState({ endAnimation: false });
+        }, 150);
+
         pauseAnimation = true;
         setTimeout(() => {
-          this.setState({pauseAnimation: false});
-        }, 20);
+          this.setState({ pauseAnimation: false });
+        }, 170);
       }
       this.setState({
         dragX: x,
         dragY: y,
-        pauseAnimation: pauseAnimation
+        pauseAnimation: pauseAnimation,
+        endAnimation: endAnimation
       });
     }
 
-    onDrag = (e) => {
-      if(this.props.dragManager){
-        this.props.dragManager.startDrag(e, this.domNode, this.props.item, this.updateDrag.bind(this));
+    onDrag = e => {
+      if (this.props.item == null || this.props.item.disabled) return;
+      if (this.props.dragManager) {
+        this.props.dragManager.startDrag(
+          e,
+          this.domNode,
+          this.props.item,
+          this.updateDrag.bind(this)
+        );
       }
-    }
+    };
 
     getStyle() {
+      if (this.props.item == null || this.props.item.disabled)
+        return { display: "none" };
       const options = {
         itemWidth: this.props.itemWidth,
         itemHeight: this.props.itemHeight,
         verticalMargin: this.props.verticalMargin,
         zoom: this.props.zoom
       };
-      const layout = new LayoutManager(options, this.props.layoutWidth);
-      const style = layout.getStyle(this.props.index,
+      const layout = new LayoutManager(
+        options,
+        this.props.layoutWidth,
+        this.props.rtl
+      );
+      const style = layout.getStyle(
+        this.props.index,
         this.props.animation,
-        this.props.item[this.props.filterProp]);
+        this.props.item[this.props.filterProp]
+      );
       //If this is the object being dragged, return a different style
-      if (this.props.dragManager.dragItem &&
-          this.props.dragManager.dragItem[this.props.keyProp] === this.props.item[this.props.keyProp]) {
-        const dragStyle = this.props.dragManager.getStyle(this.state.dragX, this.state.dragY);
-        return {...style, ...dragStyle};
+      if (
+        this.props.dragManager.dragItem &&
+        this.props.dragManager.dragItem[this.props.keyProp] ===
+          this.props.item[this.props.keyProp]
+      ) {
+        const dragStyle = this.props.dragManager.getStyle(
+          this.state.dragX,
+          this.state.dragY
+        );
+        return { ...style, ...dragStyle };
+      } else if (this.state && this.state.endAnimation) {
+        const container = this.props.container;
+        let x = 0;
+        let y = 0;
+        if (container != null) {
+          let gridRect = container.getBoundingClientRect();
+          x = gridRect.x;
+          y = gridRect.y;
+        }
+        const endAnimationStyle = layout.getStyleForEndAnimation(
+          this.props.index,
+          this.props.animation,
+          this.props.item[this.props.filterProp],
+          x,
+          y
+        );
+        return endAnimationStyle;
       } else if (this.state && this.state.pauseAnimation) {
-        const pauseAnimationStyle = {...style};
-        pauseAnimationStyle.WebkitTransition = 'none';
-        pauseAnimationStyle.MozTransition = 'none';
-        pauseAnimationStyle.msTransition = 'none';
-        pauseAnimationStyle.transition = 'none';
+        const pauseAnimationStyle = { ...style };
+        pauseAnimationStyle.WebkitTransition = "none";
+        pauseAnimationStyle.MozTransition = "none";
+        pauseAnimationStyle.msTransition = "none";
+        pauseAnimationStyle.transition = "none";
         return pauseAnimationStyle;
       }
       return style;
@@ -72,31 +128,34 @@ export default function createDisplayObject(DisplayObject, displayProps, forceIm
 
     componentDidMount() {
       if (this.props.dragEnabled) {
-        this.domNode.addEventListener('mousedown', this.onDrag);
-        this.domNode.addEventListener('touchstart', this.onDrag);
-        this.domNode.setAttribute('data-key', this.props.item[this.props.keyProp]);
+        this.domNode.addEventListener("mousedown", this.onDrag);
+        this.domNode.addEventListener("touchstart", this.onDrag);
+        this.domNode.setAttribute(
+          "data-key",
+          this.props.item[this.props.keyProp]
+        );
       }
     }
 
     componentWillUnmount() {
       if (this.props.dragEnabled) {
         this.props.dragManager.endDrag();
-        this.domNode.removeEventListener('mousedown', this.onDrag);
-        this.domNode.removeEventListener('touchstart', this.onDrag);
+        this.domNode.removeEventListener("mousedown", this.onDrag);
+        this.domNode.removeEventListener("touchstart", this.onDrag);
       }
     }
 
     render() {
       return (
-        <div ref={node => this.domNode = node}
-             style={this.getStyle()}>
+        <div ref={node => (this.domNode = node)} style={this.getStyle()}>
           <DisplayObject
             {...displayProps}
             item={this.props.item}
             index={this.props.index}
-            itemsLength={this.props.itemsLength}/>
+            itemsLength={this.props.itemsLength}
+          />
         </div>
       );
     }
-  }
+  };
 }
